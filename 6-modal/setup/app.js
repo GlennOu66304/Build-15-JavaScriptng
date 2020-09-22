b1fc3a8713e9c1ad9ec498bcaf266f267bf0d896
GlennOu66304 <2738328919@qq.com>
2020-09-22 23:16:19 +0800

    Code reading

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -2,3 +2,26 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".closeBtn");
+
+
+modalBtn.addEventListener("click",function(){
+    modal.classList.add("open-modal")
+});
+
+closeBtn.addEventListener("click",function(){
+    modal.classList.remove("open-modal")
+});
+
+/*
+## 1.Mindset behind the code
+1.Make sure that every html content is exactly same as in the instructions video;
+2.Utilize the develpoer tool inspect the html content in the finall file in the browser and 
+the set up file in the browser;
+## 2. Resourcelist:
+1.EventTarget.addEventListener()
+2.DOMTokenList
+https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList#Examples
+*/
